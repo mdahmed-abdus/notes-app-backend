e8861f0c5a51f2558d1c8cd6f573a98e18868364
Mohd Ahmed <mdahmed.abdus@gmail.com>
2021-03-27 20:10:47 +0530

    New middleware: catch async errors

--- routes/notes.js
+++ routes/notes.js
@@ -2,70 +2,83 @@ const express = require('express');
 const { Note } = require('../models/Note');
 const { NotFound } = require('../errors/customErrors');
 const validateId = require('../middleware/validateId');
+const catchAsyncErr = require('../middleware/catchAsyncErr');
 
 const router = express.Router();
 
 // validate id provided in params
 router.param('id', validateId);
 
-router.post('/', async (req, res) => {
-  const note = new Note({ ...req.body, ownerId: req.session.userId });
+router.post(
+  '/',
+  catchAsyncErr(async (req, res) => {
+    const note = new Note({ ...req.body, ownerId: req.session.userId });
 
-  await note.save();
+    await note.save();
 
-  res.status(201).json({ success: true, message: 'Note created', note });
-});
+    res.status(201).json({ success: true, message: 'Note created', note });
+  })
+);
 
-router.get('/:id', async (req, res) => {
-  const note = await Note.findById(req.params.id);
+router.get(
+  '/:id',
+  catchAsyncErr(async (req, res) => {
+    const note = await Note.findById(req.params.id);
 
-  if (!note) {
-    throw new NotFound('Note with given id was not found');
-  }
+    if (!note) {
+      throw new NotFound('Note with given id was not found');
+    }
 
-  if (!note.isOwner(req.session.userId)) {
-    throw new NotFound('Note with given id was not found');
-  }
+    if (!note.isOwner(req.session.userId)) {
+      throw new NotFound('Note with given id was not found');
+    }
 
-  res.json({ success: true, message: 'Note found', note });
-});
+    res.json({ success: true, message: 'Note found', note });
+  })
+);
 
-router.put('/:id', async (req, res) => {
-  const note = await Note.findById(req.params.id);
+router.put(
+  '/:id',
+  catchAsyncErr(async (req, res) => {
+    const note = await Note.findById(req.params.id);
 
-  if (!note) {
-    throw new NotFound('Note with given id was not found');
-  }
+    if (!note) {
+      throw new NotFound('Note with given id was not found');
+    }
 
-  if (!note.isOwner(req.session.userId)) {
-    throw new NotFound('Note with given id was not found');
-  }
+    if (!note.isOwner(req.session.userId)) {
+      throw new NotFound('Note with given id was not found');
+    }
 
-  // if updated title not provided then use the same title
-  // if updated body not provided then use the same body
-  const { title = note.title, body = note.body } = req.body;
+    // if updated title not provided then use the same title
+    // if updated body not provided then use the same body
+    const { title = note.title, body = note.body } = req.body;
 
-  note.title = title;
-  note.body = body;
-  await note.save();
+    note.title = title;
+    note.body = body;
+    await note.save();
 
-  res.json({ success: true, message: 'Note updated', note });
-});
+    res.json({ success: true, message: 'Note updated', note });
+  })
+);
 
-router.delete('/:id', async (req, res) => {
-  const note = await Note.findById(req.params.id);
+router.delete(
+  '/:id',
+  catchAsyncErr(async (req, res) => {
+    const note = await Note.findById(req.params.id);
 
-  if (!note) {
-    throw new NotFound('Note with given id was not found');
-  }
+    if (!note) {
+      throw new NotFound('Note with given id was not found');
+    }
 
-  if (!note.isOwner(req.session.userId)) {
-    throw new NotFound('Note with given id was not found');
-  }
+    if (!note.isOwner(req.session.userId)) {
+      throw new NotFound('Note with given id was not found');
+    }
 
-  await note.remove();
+    await note.remove();
 
-  res.json({ success: true, message: 'Note deleted', note });
-});
+    res.json({ success: true, message: 'Note deleted', note });
+  })
+);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,51 +3,64 @@ const _ = require('lodash');
 const { User } = require('../models/User');
 const authService = require('../services/authService');
 const { BadRequest } = require('../errors/customErrors');
+const catchAsyncErr = require('../middleware/catchAsyncErr');
 
 const router = express.Router();
 
-router.get('/profile', async (req, res) => {
-  const user = await User.findById(req.session.userId).select('-password');
-  res.json({ success: true, message: 'User details', user });
-});
+router.get(
+  '/profile',
+  catchAsyncErr(async (req, res) => {
+    const user = await User.findById(req.session.userId).select('-password');
+    res.json({ success: true, message: 'User details', user });
+  })
+);
 
-router.post('/register', async (req, res) => {
-  const userExists = await User.exists({ email: req.body.email });
+router.post(
+  '/register',
+  catchAsyncErr(async (req, res) => {
+    const userExists = await User.exists({ email: req.body.email });
 
-  if (userExists) {
-    throw new BadRequest('User already registered');
-  }
+    if (userExists) {
+      throw new BadRequest('User already registered');
+    }
 
-  const user = new User(req.body);
-  await user.save();
+    const user = new User(req.body);
+    await user.save();
 
-  res.status(201).json({
-    success: true,
-    message: 'User registered',
-    user: _.pick(user, ['_id', 'name', 'email', 'createdAt']),
-  });
-});
+    res.status(201).json({
+      success: true,
+      message: 'User registered',
+      user: _.pick(user, ['_id', 'name', 'email', 'createdAt']),
+    });
+  })
+);
 
-router.post('/login', async (req, res) => {
-  const user = await User.findOne({ email: req.body.email });
+router.post(
+  '/login',
+  catchAsyncErr(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
 
-  if (!user) {
-    throw new BadRequest('Invalid email or password');
-  }
+    if (!user) {
+      throw new BadRequest('Invalid email or password');
+    }
 
-  const validPassword = await user.comparePassword(req.body.password);
+    const validPassword = await user.comparePassword(req.body.password);
 
-  if (!validPassword) {
-    throw new BadRequest('Invalid email or password');
-  }
+    if (!validPassword) {
+      throw new BadRequest('Invalid email or password');
+    }
 
-  authService.login(req, user._id);
+    authService.login(req, user._id);
 
-  res.json({ success: true, message: 'User logged in' });
-});
+    res.json({ success: true, message: 'User logged in' });
+  })
+);
 
-router.post('/logout', (req, res) => {
-  authService.logout(req, res);
-});
+router.post(
+  '/logout',
+  catchAsyncErr((req, res) => {
+    authService.logout(req, res);
+  })
+);
 
 module.exports = router;
