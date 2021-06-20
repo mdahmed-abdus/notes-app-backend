10be21ed55e864a1d566e7a517a0ac69831f2ea6
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2021-06-20 09:14:11 +0530

    Add new dev dependencie: morgan

--- middleware/appMiddleware.js
+++ middleware/appMiddleware.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
+const { IN_PROD } = require('../config/appConfig');
 const { SESSION_OPTIONS } = require('../config/sessionConfig');
 const { MONGO_STORE_OPTIONS } = require('../config/mongoStoreConfig');
 
 module.exports = app => {
   // body parser
   app.use(express.json());
+
+  if (!IN_PROD) {
+    app.use(morgan('dev'));
+  }
 
   // session
   app.use(
