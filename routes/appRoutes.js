e6ac3c77fd775f5981f441018a95cf52c8f66b19
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2021-06-20 10:34:36 +0530

    Add invalid route handler

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,7 +1,9 @@
 const users = require('./users');
 const notes = require('./notes');
+const notFound = require('../errors/notFound');
 
 module.exports = app => {
   app.use('/users', users);
   app.use('/notes', notes);
+  app.use(notFound);
 };
