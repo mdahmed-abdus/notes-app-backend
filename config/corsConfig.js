959f57ebeae6f0059a5b819e28d5d0dce16954a4
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2021-07-09 11:37:01 +0530

    Add cors

---
+++ config/corsConfig.js
@@ -0,0 +1,3 @@
+const corsOptions = { origin: 'http://localhost:3000', credentials: true };
+
+module.exports = { corsOptions };
--- middleware/appMiddleware.js
+++ middleware/appMiddleware.js
@@ -2,11 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
+const cors = require('cors');
 const { IN_PROD } = require('../config/appConfig');
 const { SESSION_OPTIONS } = require('../config/sessionConfig');
 const { MONGO_STORE_OPTIONS } = require('../config/mongoStoreConfig');
+const { corsOptions } = require('../config/corsConfig');
 
 module.exports = app => {
+  // cors
+  app.use(cors(corsOptions));
+
   // body parser
   app.use(express.json());
 
