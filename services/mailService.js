b7acc9b14c1a61bd63a036e0ce9be42f62ef77f6
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2023-09-25 17:14:56 +0530

    Add resend for sending mails in prod

--- services/mailService.js
+++ services/mailService.js
@@ -1,8 +1,23 @@
 const nodemailer = require('nodemailer');
+const { Resend } = require('resend');
+const { IN_PROD } = require('../config/appConfig');
 const { SMTP_OPTIONS, FROM } = require('../config/mailConfig');
 
+// smtp
 const transporter = nodemailer.createTransport(SMTP_OPTIONS);
 
-const sendMail = options => transporter.sendMail({ ...options, from: FROM });
+// resend
+
+const smtpSendMail = options =>
+  transporter.sendMail({ ...options, from: FROM });
+
+// resend
+const resend = new Resend(RESEND_API_KEY);
+
+const resendSendMail = options =>
+  resend.emails.send({ ...options, from: RESEND_FROM });
+
+// use smtp for development and resend for production
+const sendMail = IN_PROD ? resendSendMail : smtpSendMail;
 
 module.exports = { sendMail };
